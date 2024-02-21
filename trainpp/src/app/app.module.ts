bc0090019b6882bc62298c8a6440cab8afc556a8
Ravindu Balasooriya <ravindhuprabasha@gmail.com>
2024-02-21 11:47:51 +0530

    styles (Prime) working

--- trainpp/src/app/app.module.ts
+++ trainpp/src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { HomeComponent } from './components/home/home.component';
+import { CardModule } from 'primeng/card';
 
 @NgModule({
   declarations: [
@@ -16,7 +18,8 @@ import { HomeComponent } from './components/home/home.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CardModule
   ],
   providers: [
     provideClientHydration()
