52d7e0e2f8d2e5dbbe7ee065b3e65fe680c802e6
Ravindu Balasooriya <ravindhuprabasha@gmail.com>
2024-02-26 11:39:23 +0530

    merged with dev after login comptd

--- trainpp/src/app/app.module.ts
+++ trainpp/src/app/app.module.ts
@@ -1,7 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
-
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
@@ -11,11 +9,8 @@ import { CardModule } from 'primeng/card';
 import { InputTextModule } from 'primeng/inputtext';
 import { ReactiveFormsModule } from '@angular/forms';
 import { ButtonModule } from 'primeng/button';
-import { AvatarModule } from 'primeng/avatar';
 import { AvatarGroupModule } from 'primeng/avatargroup';
 import { SidebarModule } from 'primeng/sidebar';
-
-
 import { LayoutComponent } from './layout/layout.component';
 import { ToolbarModule } from 'primeng/toolbar';
 import { AvatarModule } from 'primeng/avatar';
