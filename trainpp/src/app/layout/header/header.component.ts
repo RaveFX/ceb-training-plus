c2a72a9ccda20dc751717978a3e94bbc3a3c8e52
AnushkaD99 <92775945+AnushkaD99@users.noreply.github.com>
2024-04-03 09:44:36 +0530

    link added

--- trainpp/src/app/layout/header/header.component.ts
+++ trainpp/src/app/layout/header/header.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+  role: string | null = sessionStorage.getItem('role');
+  profileLink: string = "";
 
+  constructor() {
+    if (this.role === "TO") {
+      this.profileLink = "/to/profile";
+    } else if (this.role === "DGM") {
+      this.profileLink = "/dgm/profile";
+    }
+  }
 }
