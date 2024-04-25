c5fa9f9e5303f6e4577dfe83c6d2e5873c0f6593
AnushkaD99 <92775945+AnushkaD99@users.noreply.github.com>
2024-04-25 09:33:04 +0530

    attendance UI 1 done

--- trainpp/src/app/pages/attendance-to/attendance-to.component.ts
+++ trainpp/src/app/pages/attendance-to/attendance-to.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Program, PROGRAMS } from './data/trainings-data';
 
 @Component({
   selector: 'app-attendance-to',
@@ -6,5 +7,33 @@ import { Component } from '@angular/core';
   styleUrl: './attendance-to.component.css'
 })
 export class AttendanceToComponent {
+  programs: Program[] = PROGRAMS;
+  statuses!: any[];
+  onStatusChange(selectedValues: any[]) {
+    console.log('Selected Status Values:', selectedValues);
+    // Other logic if needed
+  }
 
+  getSeverity(status: string): string {
+    if (status === 'pending') {
+      return 'warning';
+    } else if (status === 'on going') {
+      return 'success';
+    } else {
+      return 'info';
+    }
+ }
+
+ getOngoingPrograms(): Program[] {
+  return this.programs.filter(program => program.status === 'on going');
+}
+
+  constructor() {}
+
+  ngOnInit() {
+    this.statuses = [
+      { label: 'On Going', value: 'on going' },
+      { label: 'Pending', value: 'pending' },
+    ];
+  }
 }
